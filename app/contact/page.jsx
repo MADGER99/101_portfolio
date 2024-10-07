631d2c7a40925952cf0d4befec2688215d40afef
MADGER <MADKINGX3X@Gmail.com>
2024-10-07 17:42:01 +0700

    V1

---
+++ app/contact/page.jsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import { Select,SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+66) 943 697 891",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "Madger@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    description: ".......",
+  },
+]
+
+import { motion } from "framer-motion";
+
+const Content = () => {
+  return (
+    <motion.section
+      initial={{ opacity: 0}}
+      animate={{
+        opacity:1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn"},
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px] w-full ">
+
+          {/* form */}
+          <div className="xl:w-[60%] order-2 xl:order-none ">
+            <form className="flex flex-col gap-6 p-10 bg-pink-50/10 rounded-xl">
+            <h3 className="text-4xl text-accent ">Let's work together</h3>
+            <p className="text-white/60">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente delectus nam nemo consectetur quaerat ducimus earum. Sed nostrum a nulla quam ab! Eligendi velit corporis laboriosam perferendis neque veritatis doloribus?
+            </p>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                  <Input type="firstname" placeholder="Firstname" />
+                  <Input type="lastname" placeholder="Lastname" />
+                  <Input type="email" placeholder="Email address" />
+                  <Input type="phone" placeholder="Phone number" />
+                </div>
+                <Select>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select a Service" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel > Select a Service </SelectLabel>
+                      <SelectItem value ="est">Web Development</SelectItem>
+                      <SelectItem value ="cst">UI/UX Design</SelectItem>
+                      <SelectItem value ="mst">Logo Design</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                {/* textarea */}
+                <Textarea 
+                  className="h-[200px]" 
+                  placeholder="Type your massage here."
+                />
+                {/* button */}
+                <Button  className="max-w-60 max-h-40 ">
+                  Send message
+                </Button>
+            </form>
+          </div>
+
+          {/* info */}
+          <div className="flex-1 flex items-center xl:justify-start order-1 xl:order-none mb-8 xl:mb-0 ">
+            <ul className="flex flex-col gap-10 ">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-pink-50/10 text-accent rounded-lg flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </motion.section>
+  )
+}
+
+export default Content
---
+++ app/page.jsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button"
+import { FiDownload } from "react-icons/fi"
+///////import-components//////////
+import Social from "@/components/ui/Social"
+import Photo from "@/components/Photo"
+import Stats from "@/components/Stats"
+//////////////////////////////////
+export default function Home() {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto ">
+        <div className="container flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+
+        {/* text homepage */}
+        <div className="text-center xl:text-left order-2 xl:order-none">
+          <span className="text-xl">Software  Developer</span>
+          <h1 className="h1 mb-6">
+            Hello I'm <br/> <span className="text-accent">Surasak Jantapan</span>
+          </h1>
+          <p className="max-w-[500px] mb-9 text-white/80">
+            &nbsp;&nbsp;ผมมีความสามารถในด้าน Wep Programming เทคโนโลยีต่างที่ผมใช้ในการพัฒนา เช่น React.js Next.Js html5 Css3 javascript Node.Js Tailwind
+            และกำลังจะพัฒนาไปเป็น Full Stack.
+          </p>
+          {/* btn & Socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              
+              <Button 
+                variant="outline"
+                className="uppercase flex items-center gap-2"
+                >
+                <span>Download Cv</span>
+                <FiDownload className="text-xl"/>
+              </Button>
+              
+              <div className="mb-8 xl:mb-0">
+                <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex 
+                justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
+                </div>
+            </div>
+        </div>
+
+        {/* Photo */}
+        <div className="order-1 xl:order-none mb-30 xl:mb-0">
+          <Photo />
+        </div>
+        </div>
+      </div>
+      <Stats /> 
+    </section>
+  );
+}
